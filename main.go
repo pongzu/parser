7320bec602e1b5f1201b2654dec2aa263e13694d
posuke0623 <yuyuxx0623@gmail.com>
2018-12-14 19:06:52 +0900

    fixed

--- main.go
+++ main.go
@@ -1,32 +1,46 @@
 package main
 
 import (
-	"bufio"
+	"go/ast"
+	"go/parser"
+	"go/token"
 	"log"
 	"os"
 )
 
 func main() {
-
-	if len(os.Args) > 2 {
-		log.Fatal("コマンドライン引数が多すぎる")
+	if len(os.Args) != 2 {
+		log.Fatal("コマンドライン引数が間違っている")
 	}
 
-	// コマンドライン引数に指定したファイル名のスキャナーを作成
-	scanner, err := getScanner(os.Args[1])
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	parse(scanner)
 }
 
-func getScanner(name string) (*bufio.Scanner, error) {
-	file, err := os.Open(name)
+func run() error {
+
+	//コマンラインで受け取った名前のファイルを開く
+	f, err := getFile()
+	if err != nil {
+		return err
+	}
+
+	//ファイルをパーサーからパースする
+	fs := token.NewFileSet()
+
+	file, err := parser.ParseFile(fs, f.Name(), f, parser.Mode(0))
+	if err != nil {
+		return err
+	}
+
+	for _, v := range file.Decls {
+		ast.Print(fs, v)
+	}
+	return nil
+}
+
+func getFile() (*os.File, error) {
+	file, err := os.Open(os.Args[1])
 	if err != nil {
 		return nil, err
 	}
-	defer file.Close()
-
-	return bufio.NewScanner(file), nil
+	return file, nil
 }
