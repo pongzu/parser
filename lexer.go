b98db0bb87826f1fdb6806ddbbd704fede55f141
posuke0623 <yuyuxx0623@gmail.com>
2018-12-17 18:36:41 +0900

    レキサーメソッドを定義し、今からパーサーを綺麗に書く

---
+++ lexer.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bytes"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"io"
+	"os"
+)
+
+// ASTに分解して、取得できた文字列をスライスにいれて返すメソッド
+func lex(src io.Reader) ([]string, error) {
+	var (
+		node *ast.File
+		err  error
+	)
+	fset := token.NewFileSet()
+
+	if file, ok := src.(*os.File); ok {
+		node, err = parser.ParseFile(fset, file.Name(), nil, parser.Mode(0))
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		dst := new(bytes.Buffer)
+		io.Copy(dst, src)
+		node, err = parser.ParseFile(fset, "file.go", dst, parser.Mode(0))
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	var strings []string
+
+	ast.Inspect(node, func(n ast.Node) bool {
+		switch x := n.(type) {
+		case *ast.BasicLit:
+			strings = append(strings, x.Value)
+		}
+		return true
+	})
+	return strings, nil
+}
--- main.go
+++ main.go
@@ -1,49 +1,28 @@
 package main
 
 import (
-	"go/ast"
-	"go/parser"
-	"go/token"
 	"log"
 	"os"
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		log.Fatal("コマンドライン引数が間違っている")
-	} 
-
-}
-
-func run() error {
-
-	//コマンラインで受け取った名前のファイルを開く
-	f, err := getFile()
-	if err != nil {
-		return err
+	if len(os.Args) < 2 {
+		log.Fatal("コマンドライン引数が足りない")
 	}
+	for i := 1; i < len(os.Args); i++ {
+		f, err := os.Open(os.Args[i])
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
 
-	//ファイルをパーサーからパースする
-	fs := token.NewFileSet()
+		// 取得してきた文字列をパースメソッドにわたす
+		strings, err := lex(f)
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	file, err := parser.ParseFile(fs, f.Name(), f, parser.Mode(0))
-	if err != nil {
-		return err
+		//parse(strings)
+
 	}
-
-	// 今から全てのノードの値を取得してきて、そこから文字列か判断して、文字列であれば、SQL文かどうか判断して、その場合は綺麗にする
-
-	for _, v := range file.Decls {
-		ast.Print(fs, v)
-	}
-	return nil
-}
-
-
-func getFile() (*os.File, error) {
-	file, err := os.Open(os.Args[1])
-	if err != nil {
-		return nil, err
-	}
-	return file, nil
 }
