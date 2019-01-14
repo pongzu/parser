7d0e4d52b7b1d12071d5a33d41e8fa446ca324e8
pongzu <yuyuxx0623@gmail.com>
2019-01-14 16:53:42 +0900

    トークンを取得するところまでは完成した

--- format.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "fmt"
-
-func formatSQL(src string) {
-	fmt.Println("今からロジック書きます")
-}
--- main.go
+++ main.go
@@ -1,28 +1,151 @@
 package main
 
 import (
-	"log"
-	"os"
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"strings"
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		log.Fatal("コマンドライン引数が足りない")
-	}
-	for i := 1; i < len(os.Args); i++ {
-		f, err := os.Open(os.Args[i])
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer f.Close()
+	var s = "select * from unko"
+	stmt := &SelectStmt{}
+	stmt.FormatSelectStmt(s)
+}
 
-		// ASTに分解して、ファイルから文字列のみを取得している
-		strings, err := lex(f)
-		if err != nil {
-			log.Fatal(err)
-		}
-		p := new(Parser)
-		// 
+type Scanner struct {
+	r *bufio.Reader
+}
 
+// SelectStmt はセレクト文をトークンに分割して、スペースも排除された状態
+type SelectStmt struct {
+	tokens []Token
+	fields []string
+}
+
+type Token int
+
+var eof = rune(0)
+
+const (
+	ILLEGAL  = iota // some token that can not recognized
+	EOF             // end of file
+	WS              // 空白
+	IDENT           //Fields,TableNameなど
+	ASTERISK        //*
+	COMMA           //,
+	SELECT          // SELECT key word
+	FROM            // FROM key word
+)
+
+func (stmt *SelectStmt) FormatSelectStmt(src string) {
+	scanner := NewScanner(strings.NewReader(src))
+
+	for {
+		token, field := scanner.ScanIgnoreWhiteSpace()
+		if token == EOF {
+			break
+		}
+		stmt.tokens = append(stmt.tokens, token)
+		stmt.fields = append(stmt.fields, field)
 	}
+	fmt.Println(stmt.tokens)
+}
+
+func isWhiteSpace(ch rune) bool {
+	return ch == ' ' || ch == '\t' || ch == '\n'
+}
+
+func isLetter(ch rune) bool {
+	return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')
+}
+
+func isDisit(ch rune) bool {
+	return ch >= '0' && ch <= '9'
+}
+
+func NewScanner(src io.Reader) *Scanner {
+	return &Scanner{r: bufio.NewReader(src)}
+}
+
+func (s *Scanner) read() rune {
+	ch, _, err := s.r.ReadRune()
+	if err != nil {
+		return eof
+	}
+	return ch
+}
+
+func (s *Scanner) unread() {
+	_ = s.r.UnreadRune()
+}
+
+func (s *Scanner) Scan() (Token, string) {
+	ch := s.read()
+	if isWhiteSpace(ch) {
+		s.unread()
+		return s.ProcessWhiteSpace()
+	} else if isLetter(ch) {
+		s.unread()
+		return s.ProcessIdent()
+	}
+
+	switch ch {
+	case eof:
+		return EOF, ""
+	case '*':
+		return ASTERISK, string(ch)
+	case ',':
+		return COMMA, string(ch)
+	}
+	return ILLEGAL, string(ch)
+}
+
+func (s *Scanner) ScanIgnoreWhiteSpace() (Token, string) {
+	tok, field := s.Scan()
+	if tok == WS {
+		tok, field = s.Scan()
+	}
+	return tok, field
+}
+
+func (s *Scanner) ProcessWhiteSpace() (Token, string) {
+	var buf bytes.Buffer
+	buf.WriteRune(s.read())
+
+	for {
+		if ch := s.read(); ch == eof {
+			break
+		} else if !isWhiteSpace(ch) {
+			s.unread()
+			break
+		} else {
+			buf.WriteRune(ch)
+		}
+	}
+	return WS, buf.String()
+}
+
+func (s *Scanner) ProcessIdent() (Token, string) {
+	var buf bytes.Buffer
+	buf.WriteRune(s.read())
+
+	for {
+		if ch := s.read(); ch == eof {
+			break
+		} else if !isLetter(ch) && !isDisit(ch) && ch != '_' {
+			s.unread()
+			break
+		} else {
+			_, _ = buf.WriteRune(ch)
+		}
+	}
+	switch strings.ToUpper(buf.String()) {
+	case "SELECT":
+		return SELECT, buf.String()
+	case "FROM":
+		return FROM, buf.String()
+	}
+	return IDENT, buf.String()
 }
