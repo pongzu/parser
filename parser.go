752b52d5b582dc9459e9839c6281986c45f5886f
pongzu <yuyuxx0623@gmail.com>
2019-01-14 16:39:42 +0900

    fixed

--- parser.go
+++ parser.go
@@ -3,63 +3,68 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"fmt"
 	"io"
-	"log"
 	"strings"
 )
 
-// Token は意味のある文字を表す
-type Token int
-
-const (
-	ILLEGALToken = iota // some token that can not recognized
-	EOF                 // end of file
-	WS                  // 空白
-	IDENT               //Fields,TableNameなど
-	ASTERISK            //*
-	COMMA               //,
-	SELECT              // SELECT key word
-	FROM                // FROM key word
-)
+type Scanner struct {
+	r *bufio.Reader
+}
 
 // SelectStmt はセレクト文をトークンに分割して、スペースも排除された状態
 type SelectStmt struct {
-	tokens []int
+	tokens []Token
+	fields []string
 }
 
-type Scanner struct {
-	r *bufio.Reader
+type Token int
+
+var eof = rune(0)
+
+const (
+	ILLEGAL  = iota // some token that can not recognized
+	EOF             // end of file
+	WS              // 空白
+	IDENT           //Fields,TableNameなど
+	ASTERISK        //*
+	COMMA           //,
+	SELECT          // SELECT key word
+	FROM            // FROM key word
+)
+
+func (stmt *SelectStmt) FormatSelectStmt(src string) {
+	scanner := NewScanner(strings.NewReader(src))
+
+	var counter int
+	for {
+		if stmt.tokens[counter] == EOF {
+			break
+		}
+		token, field := scanner.Scan()
+		stmt.tokens = append(stmt.tokens, token)
+		stmt.fields = append(stmt.fields, field)
+	}
+	fmt.Println(stmt.fields)
+}
+
+func isWhiteSpace(ch rune) bool {
+	return ch == ' ' || ch == '\t' || ch == '\n'
+}
+
+func isLetter(ch rune) bool {
+	return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')
+}
+
+func isDisit(ch rune) bool {
+	return ch >= '0' && ch <= '9'
 }
 
 func NewScanner(src io.Reader) *Scanner {
 	return &Scanner{r: bufio.NewReader(src)}
 }
 
-func (stmt *SelectStmt) parseSelectStmt(src string) {
-	scanner := NewScanner(strings.NewReader(src))
-}
-
-func (s *Scanner) scan() (tok Token, lit string) {
-	ch := s.read()
-
-	switch {
-	case isWhiteSpace(ch):
-		p.unread()
-		return p.scanWhiteSpace()
-	case isLetter(ch):
-		return p.ScanIdent()
-	case ch == '*':
-		return ASTERISK, string(ch)
-	case ch == ',':
-		return COMMA, string(ch)
-	case ch == '0':
-		return EOF, string(ch)
-	}
-	return ILLEGALToken, string(ch)
-}
-
 func (s *Scanner) read() rune {
-	eof := rune(0)
 	ch, _, err := s.r.ReadRune()
 	if err != nil {
 		return eof
@@ -67,33 +72,40 @@ func (s *Scanner) read() rune {
 	return ch
 }
 
-func (s *Scanner) unread() error {
-	if err := p.r.UnreadRune(); err != nil {
-		return err
+func (s *Scanner) unread() {
+	_ = s.r.UnreadRune()
+}
+
+func (s *Scanner) Scan() (Token, string) {
+	ch := s.read()
+	if isWhiteSpace(ch) {
+		s.unread()
+		return s.ProcessWhiteSpace()
+	} else if isLetter(ch) {
+		s.unread()
+		return s.ProcessIdent()
 	}
-	return nil
+
+	switch ch {
+	case eof:
+		return EOF, ""
+	case '*':
+		return ASTERISK, string(ch)
+	case ',':
+		return COMMA, string(ch)
+	}
+	return ILLEGAL, string(ch)
 }
 
-func isWhiteSpace(ch rune) bool {
-	return ch == ' ' || ch == '\t' || ch == "\n"
-}
-
-func (s *Scanner) scanWhiteSpace() (tok Token, lit string) {
+func (s *Scanner) ProcessWhiteSpace() (Token, string) {
 	var buf bytes.Buffer
-
-	// ここで発生するえらーはそのままログに出しておいたらいいレベル
-	_, err := buf.WriteRune(p.read())
-	if err != nil {
-		log.Println(err)
-	}
+	buf.WriteRune(s.read())
 
 	for {
-		ch := p.read()
-
-		if ch == '0' {
+		if ch := s.read(); ch == eof {
 			break
 		} else if !isWhiteSpace(ch) {
-			p.unread()
+			s.unread()
 			break
 		} else {
 			buf.WriteRune(ch)
@@ -102,34 +114,20 @@ func (s *Scanner) scanWhiteSpace() (tok Token, lit string) {
 	return WS, buf.String()
 }
 
-func isLetter(ch rune) bool {
-	return (ch > 'a' && ch < 'z') || (ch > 'A' && ch < Z) || (ch > 'あ' && ch < 'ん')
-}
-
-func isDigit(ch rune) bool {
-	return (ch >= '0' && ch <= '9')
-}
-
-func (s *Scanner) ScanIdent() (tok Token, lit string) {
+func (s *Scanner) ProcessIdent() (Token, string) {
 	var buf bytes.Buffer
-	ch := p.read()
-	_, err := buf.WriteRune(ch)
-	if err != nil {
-		log.Println(err)
-	}
+	buf.WriteRune(s.read())
 
 	for {
-		ch := p.read()
-		if ch == '0' {
+		if ch := s.read(); ch == eof {
 			break
-		} else if !isLetter(ch) && !isDigit(ch) {
+		} else if !isLetter(ch) && !isDisit(ch) && ch != '_' {
 			s.unread()
 			break
 		} else {
-			_, _ = buf.WriteRune(r)
+			_, _ = buf.WriteRune(ch)
 		}
 	}
-
 	switch strings.ToUpper(buf.String()) {
 	case "SELECT":
 		return SELECT, buf.String()
